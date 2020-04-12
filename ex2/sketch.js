f3e4dd19fb79221cb8a26d0548deb86d5538a54b
Σαββας Φιλιππιδης <filippides.savvas@gmail.com>
2020-04-12 19:24:22 +0300

    changed a variable name

--- ex2/sketch.js
+++ ex2/sketch.js
@@ -1,19 +1,19 @@
 let frequencies;
 let numberOfColumns;
-let columnHSize;
+let columnWidth;
 
 function setup() {
   createCanvas(400, 400);
   numberOfColumns = 10;
   frequencies = new Array(numberOfColumns).fill(0);
-  columnHSize = width / numberOfColumns;
+  columnWidth = width / numberOfColumns;
 }
 
 function draw() {
   background(220);
   stroke(0);
   for (i = 0; i < numberOfColumns; i++)
-    rect(i * columnHSize, height, columnHSize, -frequencies[i]);
+    rect(i * columnWidth, height, columnWidth, -frequencies[i]);
     
   frequencies[floor(random(numberOfColumns))]++;
   
