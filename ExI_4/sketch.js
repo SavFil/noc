0a443e52548fbb9404eedc785823cb5b42527d59
Σαββας Φιλιππιδης <filippides.savvas@gmail.com>
2020-04-15 19:06:04 +0300

    Deleted some unnecesary lines of code in Exercise_4

--- ExI_4/sketch.js
+++ ExI_4/sketch.js
@@ -4,7 +4,7 @@ let pg;
 function setup() {
   createCanvas(400, 400);
   pg = createGraphics(400, 400);
-  slider = createSlider(0,100);
+  slider = createSlider(0, 100);
   background(220);
 }
 
@@ -13,13 +13,11 @@ function draw() {
   let val = slider.value();
   dotPositionX = randomGaussian(200, val);
   dotPositionY = randomGaussian(200, val);
-  dotPositionZ = randomGaussian(200, val);
   noStroke();
-  //colorMode(HSB);
-  fill(0,10);
+  fill(0, 10);
   ellipse(dotPositionX, dotPositionY, 10, 10);
- 
- 
+
+
 
 
 }
