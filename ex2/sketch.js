4246a61ad5f4e39be542a7233f6cad1f9d350f79
Σαββας Φιλιππιδης <filippides.savvas@gmail.com>
2020-04-17 21:21:17 +0300

    added 2 custom distribution function for barchart

--- ex2/sketch.js
+++ ex2/sketch.js
@@ -4,7 +4,7 @@ let columnWidth;
 
 function setup() {
   createCanvas(400, 400);
-  numberOfColumns = 10;
+  numberOfColumns = 40;
   frequencies = new Array(numberOfColumns).fill(0);
   columnWidth = width / numberOfColumns;
 }
@@ -14,7 +14,29 @@ function draw() {
   stroke(0);
   for (i = 0; i < numberOfColumns; i++)
     rect(i * columnWidth, height, columnWidth, -frequencies[i]);
-    
-  frequencies[floor(random(numberOfColumns))]++;
-  
+
+  //frequencies[floor(random(numberOfColumns))]++;
+  frequencies[custom()]++;
+
+}
+
+function acceptreject() {
+  while (true) {
+    let r1 = floor(random(numberOfColumns));
+    let probability = r1;
+    let r2 = floor(random(numberOfColumns));
+    if (r2 < probability) {
+      return r1;
+    }
+  }
+}
+function custom() {
+  while (true) {
+    let r1 = floor(random(numberOfColumns));
+    let probability = r1^10;
+    let r2 = floor(random(numberOfColumns));
+    if (r2 < probability) {
+      return r1;
+    }
+  }
 }
