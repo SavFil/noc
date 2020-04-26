746857d81bccd94bd5eb27f06bcf186e44f0a66f
Σαββας Φιλιππιδης <filippides.savvas@gmail.com>
2020-04-26 19:08:48 +0300

    A bit of refactoring for Exercise I-10
    Moved code for drawing in draw function and added noLoop

--- ExI-10/sketch.js
+++ ExI-10/sketch.js
@@ -1,15 +1,13 @@
 let scale = 20;
 let grid = [];
+let hSize,vSize;
 function setup() {
   createCanvas(800, 800, WEBGL);
-  background(220);
-  translate(0, 0);
-  rotateX(2 * PI / 5);
-  rotateZ(PI / 6)
-  translate(-width / 2, -height / 2);
+  
+  
   let yOffset = 1000;
-  const hSize = width / scale;
-  const vSize = height / scale;
+  hSize = width / scale;
+  vSize = height / scale;
   for (let y = 0; y < vSize; y++) {
     let xOffset = 0;
     for (let x = 0; x < hSize; x++) {
@@ -18,6 +16,15 @@ function setup() {
     }
     yOffset += 0.1;
   }
+
+}
+
+function draw() {
+  translate(0, 0);
+  rotateX(2 * PI / 5);
+  rotateZ(PI / 6)
+  translate(-width / 2, -height / 2);
+  background(220)
   beginShape(TRIANGLE_STRIP);
   for (let y = 0; y < vSize; y++) {
     for (let x = 0; x < hSize; x++) {
@@ -28,7 +35,5 @@ function setup() {
     }
     endShape();
   }
-}
-
-function draw() {
+  noLoop();
 }
