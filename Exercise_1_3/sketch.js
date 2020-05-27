ebeb96ff595a23aa4b7c4864c7d0602a9a4c8d34
Σαββας Φιλιππιδης <filippides.savvas@gmail.com>
2020-05-27 19:57:46 +0300

    Exercise_1_3 some progress

--- Exercise_1_3/sketch.js
+++ Exercise_1_3/sketch.js
@@ -2,10 +2,11 @@ let position;
 let velocity;
 
 function setup() {
-    createCanvas(640, 360, WEBGL);
+    createCanvas(640, 640, WEBGL);
 
-    position = createVector(100, 100, 0);
-    velocity = createVector(2.5, 5, 1);
+    position = createVector(0, 0, 0);
+    velocity = createVector(-2.5, -5, 1);
+
   }
   
   function draw() {
@@ -13,20 +14,21 @@ function setup() {
 
     position.add(velocity);
 
-    if((position.x>width)||(position.x<0)){
+    if((position.x>width/2)||(position.x<-width/2)){
         velocity.x = velocity.x * -1;
     }
-    if((position.y > height ) || (position.y<0)){
+    if((position.y > height/2 ) || (position.y<-height/2)){
         velocity.y = velocity.y * -1;
     }
-    if((position.z > 100 ) || (position.y<-100)){
+    if((position.z > height/2 ) || (position.z<-height/2)){
         velocity.z = velocity.z * -1;
     }
     
+    console.log(position.x);
     push();
     translate(position.x,position.y,position.z);
     stroke(0);
-    nofill();
+    
     sphere(50);
     pop();
   }
